7bf2d33af37515e675037817775d1880169de814
Alireza <seyedalhae@gmail.com>
2024-02-20 13:42:12 +0330

    Add sendWithPattern & sendWithRetry methods

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const apiKey = process.env.API_KEY;
+const apiKeyWithoutPattern = process.env.API_KEY_WITHOUT_PATTERN;
+const apiKeyWithPattern = process.env.API_KEY_WITH_PATTERN;
 const sampleMobile = process.env.SAMPLE_MOBILE;
 const sampleMobile2 = process.env.SAMPLE_MOBILE2;
 const sampleMobile3 = process.env.SAMPLE_MOBILE3;
@@ -15,28 +16,32 @@ interface IData {
 }
 
 export class Sms1ir {
-	private apiKey: string;
+	private apiKeyWithoutPattern: string;
+	private apiKeyWithPattern: string | null;
 	private apiUrl: string = "https://app.sms1.ir:7001/api/service/";
 	private maxRetries: number = 3;
 	private retryInterval: number = 1000;
-
-	constructor(apiKey: string) {
-		this.apiKey = apiKey;
+	constructor(apiKeyWithoutPattern: string, apiKeyWithPattern?: string) {
+		this.apiKeyWithoutPattern = apiKeyWithoutPattern;
+		this.apiKeyWithPattern = apiKeyWithPattern ?? null;
 	}
 
 	private async Api(
 		urlSuffix: string,
 		method: "GET" | "POST" | "DELETE" = "POST",
-		data: object | undefined = undefined
+		data: object | undefined = undefined,
+		usePatternApiKey: boolean = false
 	): Promise<IData> {
 		const url = `${this.apiUrl}${urlSuffix}`;
-		console.log("API URL:", url);
+		const apiKey = usePatternApiKey
+			? this.apiKeyWithPattern
+			: this.apiKeyWithoutPattern;
 
 		const response = await fetch(url, {
 			headers: {
 				Accept: "application/json",
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${this.apiKey}`,
+				Authorization: `Bearer ${apiKey}`,
 			},
 			method,
 			body: JSON.stringify(data),
@@ -90,7 +95,7 @@ export class Sms1ir {
 	async sendVerificationCode(verificationCode: string, recipient: string) {
 		const message = `Your verification code is: ${verificationCode}`;
 		try {
-			const responseBody = await this.send(message, recipient);
+			const responseBody = await this.sendWithRetry(message, recipient);
 			return responseBody;
 		} catch (error) {
 			console.error("Error sending verification code:", error);
@@ -98,11 +103,30 @@ export class Sms1ir {
 		}
 	}
 
+	async sendWithPattern(patternId: number, recipient: string, pairs: any) {
+		try {
+			const responseBody = await this.Api(
+				"patternSend",
+				"POST",
+				{
+					patternId,
+					recipient,
+					pairs,
+				},
+				true
+			);
+			return responseBody;
+		} catch (error) {
+			console.error("Error sending SMS with pattern:", error);
+			throw new Error("Failed to send SMS with pattern");
+		}
+	}
+
 	private async sendWithRetry(
 		message: string,
 		recipient: string,
 		retries: number = 0
-	) {
+	): Promise<IData> {
 		try {
 			const responseBody = await this.Api("send", "POST", {
 				message,
@@ -138,15 +162,19 @@ app.post("/sms", async (req: Request, res: Response) => {
 		const payload = req.body;
 		console.log("payload: ", payload);
 
-		const sms = new Sms1ir(apiKey!);
+		const sms = new Sms1ir(apiKeyWithoutPattern!, apiKeyWithPattern!);
 		// const sms1 = await sms.send("Hello World!", sampleMobile3!);
 		// const sms1 = await sms.bulkSend("Hello World!", [
 		// 	sampleMobile!,
 		// 	sampleMobile2!,
 		// 	sampleMobile3!,
 		// ]);
+		// const sms1 = await sms.sendVerificationCode("12323", sampleMobile!);
+		// const sms1 = await sms.sendVerificationCode("123321", sampleMobile!);
+		const sms1 = await sms.sendWithPattern(125, "09105660150", {
+			otpCode: "987654",
+		});
 
-		const sms1 = await sms.sendVerificationCode("12323", sampleMobile!);
 		res.json({ response: sms1 });
 	} catch (error) {
 		console.error("Error in /users route:", error);
